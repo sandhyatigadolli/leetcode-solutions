125a695fd93e653c397c8d5ba92d00717deee640
Sandhya Subhas Tigadolli <167194729+sandhyatigadolli@users.noreply.github.com>
2026-03-20 17:19:01 +0530

    Time: 35 ms (43.64%), Space: 59.1 MB (83.12%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -7,7 +7,7 @@ class Solution {
             continue;
         }
      int left = i+1, right = nums.length-1;
-     while ( left< right) {
+     while (left< right) {
         int sum = nums[i] + nums[left] + nums[right];
         if(sum == 0) {
            result.add(Arrays.asList(nums[i], nums[left], nums[right]));
